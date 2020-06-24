c6e063290e6908622ce4346cd9cbc0ed60dd69d9
panzhigao <panzhigao@58.com>
2020-06-24 21:02:10 +0800

    'init'

---
+++ src/main/java/vip/panzhigao/StarterMain.java
@@ -0,0 +1,17 @@
+package vip.panzhigao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author panzhigao
+ */
+@SpringBootApplication
+public class StarterMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterMain.class,args);
+    }
+
+}
